7ca5a027d0015c58091505f329233ac23c7e4e2c
lybui <lyybui814@gmail.com>
2026-03-10 16:14:44 +0700

    fix

--- app/Models/Clients/User.php
+++ app/Models/Clients/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\clients;
+namespace App\Models\Clients;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;

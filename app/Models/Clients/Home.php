319c1c19800d70bbcd082003d8fd8855b048393b
lybui <lyybui814@gmail.com>
2026-03-10 16:01:49 +0700

    update home model

--- app/Models/Clients/Home.php
+++ app/Models/Clients/Home.php
@@ -4,7 +4,8 @@ namespace App\Models\clients;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; 
+use App\Models\clients\Tours;
 class Home extends Model
 {
     Use HasFactory;
